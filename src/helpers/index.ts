9e0de8dc4122e768179ce7314f511cce7010ae87
Pawel Gnat <pawel_gnat@o2.pl>
2024-11-11 16:39:05 +0100

    chore: format code

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,65 +1,50 @@
-import express from "express";
-import jwt from "jsonwebtoken";
-import "dotenv/config";
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
 
-import { Status } from "../types";
+import { Status } from '../types'
 
 // Factory function
 export const createResponseHelpers = (res: express.Response) => {
-  return {
-    sendFulfilledResponseWithData: (
-      object: any,
-      httpStatus: number,
-      message: string,
-      status: Status
-    ) => res.status(httpStatus).json({ object, message, status }),
+	return {
+		sendFulfilledResponseWithData: (object: any, httpStatus: number, message: string, status: Status) =>
+			res.status(httpStatus).json({ object, message, status }),
 
-    sendFulfilledResponseWithCookie: (
-      token: string,
-      httpStatus: number,
-      message: string,
-      status: Status
-    ) =>
-      res
-        .status(httpStatus)
-        .cookie("authorization", token, {
-          httpOnly: true,
-          secure: true,
-          sameSite: "none",
-          maxAge: 12 * 60 * 60 * 1000,
-          path: "/",
-        })
-        .json({ message, status }),
+		sendFulfilledResponseWithCookie: (token: string, httpStatus: number, message: string, status: Status) =>
+			res
+				.status(httpStatus)
+				.cookie('authorization', token, {
+					httpOnly: true,
+					secure: true,
+					sameSite: 'none',
+					maxAge: 12 * 60 * 60 * 1000,
+					path: '/',
+				})
+				.json({ token, message, status }),
 
-    sendFulfilledResponseWithoutData: (
-      httpStatus: number,
-      message: string,
-      status: Status
-    ) => res.status(httpStatus).json({ message, status }),
+		sendFulfilledResponseWithoutData: (httpStatus: number, message: string, status: Status) =>
+			res.status(httpStatus).json({ message, status }),
 
-    sendRejectedResponse: (
-      httpStatus: number,
-      message: string,
-      status: Status
-    ) => res.status(httpStatus).json({ error: message, status }),
-  };
-};
+		sendRejectedResponse: (httpStatus: number, message: string, status: Status) =>
+			res.status(httpStatus).json({ error: message, status }),
+	}
+}
 
 export const capitalizeFirstLetter = (string: string) => {
-  const lowerCased = string.toLowerCase();
-  return lowerCased.charAt(0).toUpperCase() + lowerCased.slice(1);
-};
+	const lowerCased = string.toLowerCase()
+	return lowerCased.charAt(0).toUpperCase() + lowerCased.slice(1)
+}
 
 export const normalizeString = (input: string) => {
-  return capitalizeFirstLetter(input.trim().replace(/\s+/g, " "));
-};
+	return capitalizeFirstLetter(input.trim().replace(/\s+/g, ' '))
+}
 
 export const generateAccessToken = (userId: { id: string }) => {
-  return jwt.sign(userId, `${process.env.ACCESS_TOKEN_SECRET}`, {
-    expiresIn: "12h",
-  });
-};
+	return jwt.sign(userId, `${process.env.ACCESS_TOKEN_SECRET}`, {
+		expiresIn: '12h',
+	})
+}
 
 export const verifyToken = (sessionToken: string) => {
-  return jwt.verify(sessionToken, `${process.env.ACCESS_TOKEN_SECRET}`);
-};
+	return jwt.verify(sessionToken, `${process.env.ACCESS_TOKEN_SECRET}`)
+}
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,73 +1,49 @@
-import express from "express";
-import jwt from "jsonwebtoken";
-import { merge, get } from "lodash";
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import { merge, get } from 'lodash'
 
-import { getSessionByToken } from "../db/sessions";
+import { getSessionByToken } from '../db/sessions'
 
-import { createResponseHelpers, verifyToken } from "../helpers";
+import { createResponseHelpers, verifyToken } from '../helpers'
 
-import { Status } from "../types";
+import { Status } from '../types'
 
-export const isAuthenticated = async (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction
-) => {
-  const responseHelpers = createResponseHelpers(res);
+export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+	const responseHelpers = createResponseHelpers(res)
 
-  try {
-    const token = req.cookies["authorization"];
+	try {
+		const token = req.cookies['authorization']
 
-    if (!token) {
-      return responseHelpers.sendRejectedResponse(
-        401,
-        "Token not present",
-        Status.DANGER
-      );
-    }
+		if (!token) {
+			return responseHelpers.sendRejectedResponse(401, 'Token not present', Status.DANGER)
+		}
 
-    try {
-      verifyToken(token);
-    } catch (error: unknown) {
-      if (error instanceof jwt.TokenExpiredError) {
-        console.error(error);
+		try {
+			verifyToken(token)
+		} catch (error: unknown) {
+			if (error instanceof jwt.TokenExpiredError) {
+				console.error(error)
 
-        return responseHelpers.sendRejectedResponse(
-          401,
-          "Token expired",
-          Status.DANGER
-        );
-      } else {
-        console.error("Invalid token:", error);
+				return responseHelpers.sendRejectedResponse(401, 'Token expired', Status.DANGER)
+			} else {
+				console.error('Invalid token:', error)
 
-        return responseHelpers.sendRejectedResponse(
-          401,
-          "Invalid token",
-          Status.DANGER
-        );
-      }
-    }
+				return responseHelpers.sendRejectedResponse(401, 'Invalid token', Status.DANGER)
+			}
+		}
 
-    const session = await getSessionByToken(token);
+		const session = await getSessionByToken(token)
 
-    if (!session) {
-      return responseHelpers.sendRejectedResponse(
-        404,
-        "Session not found",
-        Status.DANGER
-      );
-    }
+		if (!session) {
+			return responseHelpers.sendRejectedResponse(404, 'Session not found', Status.DANGER)
+		}
 
-    merge(req, { identity: session.userId });
+		merge(req, { identity: session.userId })
 
-    return next();
-  } catch (error) {
-    console.log(error);
+		return next()
+	} catch (error) {
+		console.log(error)
 
-    responseHelpers.sendRejectedResponse(
-      500,
-      "Internal server error",
-      Status.DANGER
-    );
-  }
-};
+		responseHelpers.sendRejectedResponse(500, 'Internal server error', Status.DANGER)
+	}
+}
